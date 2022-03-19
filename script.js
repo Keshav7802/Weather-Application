93aa68c6c474ff2536aab4ad77a79ffe8623dca9
Keshav7802 <65281740+Keshav7802@users.noreply.github.com>
2022-03-19 14:51:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const API_key = '32c294b9724dd792e39e2f3e9f4016a4';
 
 let dt = new Date();
-console.log(dt);
+// console.log(dt);
 
 // This function will be called every one sec.
 funcID = setInterval(() => {
@@ -41,10 +41,10 @@ document.querySelector('.search').addEventListener('keypress', function (e) {
 
 function getWeatherData() {
     navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
+        // console.log(success);
         let { latitude, longitude } = success.coords;
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=hourly,minutely&appid=${API_key}`).then(res => res.json()).then(data => {
-            console.log(data);
+            // console.log(data);
             showWeatherData(data);
         });
     });
@@ -52,10 +52,10 @@ function getWeatherData() {
 
 function getCityWeatherData(city) {
     if (city === 'Current Location') getWeatherData();
-    console.log('Executing getCityWeatherData');
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_key}`).then(resp => resp.json()).then(success => {
-        console.log(success);
-        let { lat, lon } = success[0];
+    // console.log('Executing getCityWeatherData');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`).then(resp => resp.json()).then(success => {
+        // console.log('success is ',success);
+        let { lat , lon } = success.coord;
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,minutely&appid=${API_key}`).then(res => res.json()).then(data => {
             console.log(data);
             showWeatherData(data);
@@ -143,7 +143,7 @@ function search() {
     const html = `<button class="new-button" onClick="remove()">Get back to Current Location</button>`;
     // Grab the element containing your "two" class
     var isthere = document.querySelector('.new-button');
-    console.log(isthere);
+    // console.log(isthere);
     if (isthere === null) {
         const two = document.querySelector('.searchbar');
         two.insertAdjacentHTML('afterend', html);
@@ -151,10 +151,25 @@ function search() {
 }
 
 function remove() {
-    console.log('remove is executing');
+    // console.log('remove is executing');
     const two = document.querySelector('.new-button');
     two.remove();
     getWeatherData();
+    clearInterval(funcID);
+    funcID = setInterval(() => {
+        time = new Date();
+        const month = time.getMonth();
+        const date = time.getDate();
+        const day = time.getDay();
+        const year = time.getFullYear();
+        const hour = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const _hours = (hour > 12) ? hour % 12 : hour;
+        const ampm = (hour > 12) ? 'PM' : 'AM';
+        timeEl.innerHTML = ((_hours < 10) ? ('0' + _hours) : _hours) + ':' + ((minutes < 10) ? ('0' + minutes) : minutes) + ' ' + `<span class="seconds">${((seconds < 10) ? ('0' + seconds) : seconds)}</><span id="am-pm">${ampm}</span>`;
+        dateEl.innerHTML = days[day] + ', ' + months[month] + ' ' + date + ', ' + year;
+    }, 1000);
 }
 
 function calcTime(offset) {
